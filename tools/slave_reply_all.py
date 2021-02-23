e6ca05517909b7692f14d44117b7679234904072
Simon <simon.hobbs@electrooptical.net>
2021-02-23 12:59:02 -0500

    tweaks

--- tools/slave_reply_all.py
+++ tools/slave_reply_all.py
@@ -21,17 +21,37 @@ FORMAT = ('%(asctime)-15s %(threadName)-15s'
           ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
-#log.setLevel(logging.DEBUG)
+log.setLevel(logging.DEBUG)
+#log.setLevel(logging.INFO)
 #log.setLevel(logging.WARNING)
-log.setLevel(logging.INFO)
-import copy
 
+import copy
+def write_to_reg(start, arr, string):
+    i = 0
+    while i <= len(string)//2:
+        try:
+            reg = (ord(string[2*i])<<8)
+            reg += ord(string[2*i+1])
+        except IndexError:
+            break
+        finally:
+            arr[start+i] = reg
+            assert(reg<(1<<16))
+        i+=1
+    return arr
+
+version = "MSDS Rev B."
+fv = "2.0.1"
+input_reg = [0]*1024
+input_reg = write_to_reg(33, input_reg, fv)
+input_reg = write_to_reg(1, input_reg, version)
+print(input_reg, len(input_reg))
 def run_server(device, baud=9600):
     store = ModbusSlaveContext(
         di=ModbusSequentialDataBlock(0, [17]*100),
         co=ModbusSequentialDataBlock(0, [17]*100),
         hr=ModbusSequentialDataBlock(0, [25185]*1024),
-        ir=ModbusSequentialDataBlock(0, [25185]*1024))
+        ir=ModbusSequentialDataBlock(0, copy.deepcopy(input_reg)))
     slaves = {1 : copy.deepcopy(store),
               2 : copy.deepcopy(store),
               3 : copy.deepcopy(store),
